baf95156ffca9464efff105997a60c7aee2af971
abihas <info@michaelnieuwenweg.nl>
2017-06-14 18:53:27 +0200

    added scopes openid and email

--- plugin.rb
+++ plugin.rb
@@ -35,6 +35,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
                           token_url: SiteSetting.oauth2_token_url
                         }
                         opts[:authorize_options] = SiteSetting.oauth2_authorize_options.split("|").map(&:to_sym)
+			opts[:scope] = "openid email"			
 
                         if SiteSetting.oauth2_send_auth_header?
                           opts[:token_params] = {headers: {'Authorization' => basic_auth_header }}
